ac23b478c18b53191947c9a95692c545e2b98927
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-07-06 17:59:38 -0300

    feat: tabela compra

---
+++ src/main/java/com/anderson/ecommerce/model/resource/CompraModelResource.java
@@ -0,0 +1,57 @@
+package com.anderson.ecommerce.model.resource;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "compra")
+public class CompraModelResource {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private Date data;
+    @Column(nullable = false)
+    private BigDecimal valor;
+    @Column(nullable = false)
+    private boolean finalizado;
+
+    //Get e Set
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public boolean isFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+}
