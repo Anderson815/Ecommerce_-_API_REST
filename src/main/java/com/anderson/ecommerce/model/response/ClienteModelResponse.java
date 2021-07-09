4453ece10c03d594d79fe0a46128326142394d91
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-07-09 17:39:22 -0300

    feat: model de response do Cliente

---
+++ src/main/java/com/anderson/ecommerce/model/response/ClienteModelResponse.java
@@ -0,0 +1,67 @@
+package com.anderson.ecommerce.model.response;
+
+import com.anderson.ecommerce.model.resource.CompraModelResource;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+public class ClienteModelResponse {
+
+    private String id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    @JsonIgnoreProperties("cliente")
+    private List<CompraModelResource> compras;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<CompraModelResource> getCompras() {
+        return compras;
+    }
+
+    public void setCompras(List<CompraModelResource> compras) {
+        this.compras = compras;
+    }
+}
