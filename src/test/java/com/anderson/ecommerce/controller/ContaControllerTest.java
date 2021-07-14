8aa36f49c934f230c0bdf1c2e77cb0924de415e1
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-07-14 15:54:34 -0300

    test(ClienteControllerTest): testes do método getCliente()

---
+++ src/test/java/com/anderson/ecommerce/controller/ContaControllerTest.java
@@ -0,0 +1,78 @@
+package com.anderson.ecommerce.controller;
+
+import com.anderson.ecommerce.exceptions.NotFoundException;
+import com.anderson.ecommerce.model.response.ClienteModelResponse;
+import com.anderson.ecommerce.service.ClienteService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ClienteController.class)
+public class ContaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClienteService clienteService;
+
+    private ClienteModelResponse clienteResponse;
+
+    @BeforeEach
+    public void clienteResponse(){
+        this.clienteResponse.setId("a");
+        this.clienteResponse.setNome("Anderson");
+        this.clienteResponse.setEmail("anderson@email.com");
+        this.clienteResponse.setSenha("123456");
+        this.clienteResponse.setTelefone("15987654321");
+    }
+
+    //Testes do método getCliente()
+    @Test
+    @DisplayName("getConta() com sucesso")
+    public void testGetClienteComSucesso() throws Exception{
+
+        //Parâmetro
+        String id = "a";
+
+        //Simulação
+        when(clienteService.getCliente(id))
+                .thenReturn(this.clienteResponse);
+
+        //Teste
+        mockMvc.perform(get("/cliente/{id}", id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("getConta() falha, pois não existe o cliente com o ID informado")
+    public void testGetClienteFalhaClienteNaoExiste() throws Exception{
+
+        //Parâmetro
+        String id = "b";
+
+        //Simulação
+        when(clienteService.getCliente(id))
+                .thenThrow(new NotFoundException("cliente", id));
+
+        //Teste
+        mockMvc.perform(get("/cliente/{id}", id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.mensagem", Matchers.is("Não existe cliente de id: " + id)));
+
+    }
+}
