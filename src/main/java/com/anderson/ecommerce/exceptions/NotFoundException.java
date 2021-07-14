5b6e43dff201fc7f7afed2433caecff0709c9ae7
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-07-14 11:39:29 -0300

    feat: classe NotFoundException criada

--- src/main/java/com/anderson/ecommerce/exceptions/NotFoundException.java
+++ src/main/java/com/anderson/ecommerce/exceptions/NotFoundException.java
@@ -1,4 +1,9 @@
 package com.anderson.ecommerce.exceptions;
 
 public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String id){
+        super("Não existe cliente de id: " + id);
+    }
+
 }
