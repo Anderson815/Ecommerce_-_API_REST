1b89dfb8a7d2b620d34df95d46ef06c0417e6ae1
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-09-07 16:56:23 -0300

    feat(ProdutoService): método getProduto() criado

--- src/main/java/com/anderson/ecommerce/service/ProdutoService.java
+++ src/main/java/com/anderson/ecommerce/service/ProdutoService.java
@@ -1,9 +1,34 @@
 package com.anderson.ecommerce.service;
 
+import com.anderson.ecommerce.exceptions.NotFoundException;
+import com.anderson.ecommerce.model.resource.ProdutoModelResource;
 import com.anderson.ecommerce.model.response.ProdutoModelResponse;
+import com.anderson.ecommerce.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository repository;
+
     public ProdutoModelResponse getProduto(String id) {
-        return null;
+        return this.produtoParaResposta(this.obterProduto(id));
+    }
+
+    //Métodos Auxiliares
+
+    private ProdutoModelResource obterProduto(String id){
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("produto", id));
+    }
+
+    private ProdutoModelResponse produtoParaResposta(ProdutoModelResource produtoResource){
+        return new ProdutoModelResponse(produtoResource.getId(),
+                produtoResource.getNome(),
+                produtoResource.getMarca(),
+                produtoResource.getModelo(),
+                produtoResource.getPreco(),
+                produtoResource.getEstoque());
     }
 }
